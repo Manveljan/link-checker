78bd67274ed405d263f6a839db469c8b9ab8c6bf
manvel <mvatoyan@gmail.com>
2017-01-10 15:09:39 +0400

    Done

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express')
 var router = express.Router()
 var url = require('url')
 var http = require('http')
-var cheerio = require('cheerio')
-var request = require('request')
 var pg = require('pg')
 var Promise = require('bluebird')
 var models = require('../models')
@@ -11,11 +9,7 @@ var md5 = require('md5')
 
 var lib = require('../lib/functions.js')
 
-/* GET home page. */
-
-// router.get('/', function(req, res, next) {
-//  res.json({ title: 'Express' });
-router.get('/url', function (req,res){
+router.get('/url', function (req, res) {
   var queryUrl = req.query.q
   var testpath = url.parse(queryUrl)
   var options = {
@@ -23,59 +17,27 @@ router.get('/url', function (req,res){
     port: 80,
     path: testpath.path
   }
-  console.log(testpath.host)
-  //res.json(testpath)
 
   http.get(options, function (response) {
     var body = ''
-    response.on('data', function (chunk) {
-      body += chunk
+    response.on('data', function (add) {
+      body += add
     })
     response.on('end', function () {
-      return models.url.findOne({where: {hash: md5(queryUrl)}}).then(function(result){
-        if (result){
-          console.log(result);
-          res.json({
-            status: "ok",
-            id: result.id,
-            url: queryUrl,
-            domain: testpath.host,
-            title: result.title,
-            description: result.description,
-            image_url: result.image_url
-          })
-
-        }
-        else {
-
-          var cheerioLoad = cheerio.load(body)
-          var data ={
-            hash: md5(queryUrl),
-            title: cheerioLoad('title').text(),
-            description: cheerioLoad('meta[name="description"]').attr('content'),
-            image_url:cheerioLoad('meta[property="og:image"]').attr('content')
-          }
-           lib.insertDataToDb(data).then(function(result){
-
-            res.json({
-              status: "ok",
-              id: result.id,
-              url: queryUrl,
-              domain: testpath.host,
-              title: result.title,
-              description: result.description,
-              image_url: result.image_url
-            })
-          })
-
-        }
-          console.log(result);
+      return lib.checker(queryUrl, body).then(function (result) {
+        res.json({
+          status: 'ok',
+          id: result.id,
+          url: queryUrl,
+          domain: testpath.host,
+          title: result.title,
+          description: result.description,
+          image_url: result.image_url
+        })
       })
-
     })
   }).on('error', function (e) {
-    console.log("Got error: " + e.message)
+    console.log('Got error: ' + e.message)
   })
-
 })
 module.exports = router
