3ad3205c46df17d43dc3b1c0dcdd3435c06e19a8
manvel <mvatoyan@gmail.com>
2017-01-10 18:02:41 +0400

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,31 @@
 var express = require('express')
 var router = express.Router()
 var url = require('url')
-var http = require('http')
 var pg = require('pg')
 var Promise = require('bluebird')
 var models = require('../models')
 var md5 = require('md5')
-
 var lib = require('../lib/functions.js')
 
 router.get('/url', function (req, res) {
   var queryUrl = req.query.q
-  var testpath = url.parse(queryUrl)
-  var options = {
-    host: testpath.host,
-    port: 80,
-    path: testpath.path
-  }
-
-  http.get(options, function (response) {
-    var body = ''
-    response.on('data', function (add) {
-      body += add
+  var parsedUrl = url.parse(queryUrl)
+  return Promise.resolve().then(function () {
+    if (parsedUrl.protocol === 'https:') {
+      return lib.getHttpsMeta(parsedUrl)
+    } else {
+      return lib.getHttpMeta(parsedUrl)
+    }
+  }).then(function (result) {
+    res.json({
+      status: 'ok',
+      id: result.id,
+      url: parsedUrl.href,
+      domain: parsedUrl.host,
+      title: result.title,
+      description: result.description,
+      image_url: result.image_url
     })
-    response.on('end', function () {
-      return lib.checker(queryUrl, body).then(function (result) {
-        res.json({
-          status: 'ok',
-          id: result.id,
-          url: queryUrl,
-          domain: testpath.host,
-          title: result.title,
-          description: result.description,
-          image_url: result.image_url
-        })
-      })
-    })
-  }).on('error', function (e) {
-    console.log('Got error: ' + e.message)
   })
 })
 module.exports = router
