e7dd7e451b12da87142be5fdde5887d8b3090eb4
manvel <mvatoyan@gmail.com>
2017-01-24 18:32:02 +0400

    Done

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,41 @@ var Promise = require('bluebird')
 var models = require('../models')
 var md5 = require('md5')
 var lib = require('../lib/functions.js')
+var fs = require('fs')
+
+var Canvas = require('canvas'),
+  Image = Canvas.Image,
+  canvas = new Canvas(300, 300),
+  ctx = canvas.getContext('2d')
+
+ctx.strokeStyle = 'blue'
+ctx.lineWidth = 20
+ctx.strokeRect(50, 50, 150, 150)
+ctx.font = '30px Impact'
+ctx.fillStyle = '#8A2BE2'
+ctx.fillText('aaaaaa', 70, 150)
+ctx.fillRect(80, 90, 80, 20)
+
+router.get('/', function (req, res) {
+  out = fs.createWriteStream(__dirname + '/test.png')
+  stream = canvas.pngStream()
+
+  stream.on('data', function (add) {
+    out.write(add)
+  })
+
+  stream.on('end', function () {
+    console.log('ok')
+  })
+})
 
 router.get('/url', function (req, res) {
   var queryUrl = req.query.q
   var parsedUrl = url.parse(queryUrl)
   return Promise.resolve().then(function () {
-      return lib.getMetas(parsedUrl)
+    return lib.getMetas(parsedUrl)
   }).then(function (result) {
-    console.log(result);
+    // console.log(result)
     res.json({
       status: 'ok',
       id: result.id,
